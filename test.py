6b5be3a0cc37cad8e41d728423801243175d1e2a
LarsAndreasEk <lars.andreas.ek@gmail.com>
2013-01-30 22:12:51 +0100

    La til HttpLib2

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 import os
-count = 0
-files = os.listdir("/volume1/transmission/")
-for file in files:
-	print file
-	count = count+1
 
-print count
+import httplib2
+
+http = httplib2.http()
+
+resp, content = http.request("http://www.torrentday.com/torrents/rss?download;11;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22")
+
+print content
+
+
+#count = 0
+#files = os.listdir("/volume1/transmission/")
+#for file in files:
+#	print file
+#	count = count+1
+#
+#print count
