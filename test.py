87567de045438950426f420637c3d77a1281c7bb
Lars Andreas Ek <lars.andreas.ek@gmail.com>
2013-01-31 21:53:46 +0100

    La til folder

--- test.py
+++ test.py
@@ -5,12 +5,15 @@ import feedparser
 import urllib
 
 
-def download_torrent(link, title):
-	urllib.urlretrieve (link, title.replace(" ", ".") + ".torrent")
+
+
+def download_torrent(link, title, torrent_folder):
+	urllib.urlretrieve (link, torrent_folder + "/" + title.replace(" ", ".") + ".torrent")
 
 list_of_stuff_i_want = ["Life.on.fire", "Austin.City.Limits", "Moving.On"]
 
 
+watch_folder = '/volume1/script/test_watchfolder'
 url = 'http://www.torrentday.com/torrents/rss?download;11;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22' 
 
 feed = feedparser.parse(url)
@@ -19,7 +22,7 @@ for post in feed.entries:
 	link = post.link
 	for stuff_i_like in list_of_stuff_i_want:
 		if re.match(stuff_i_like, title, re.I):
-			download_torrent(link, title)
+			download_torrent(link, title, watch_folder)
 			print "Found " + stuff_i_like + " !\n"
 			
 #	for series in title:
