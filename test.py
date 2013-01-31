c490dc62263684a9d6456be15b4533b61e9090c7
Lars Andreas Ek <lars.andreas.ek@gmail.com>
2013-01-31 21:58:43 +0100

    la til tullball

--- test.py
+++ test.py
@@ -4,17 +4,16 @@ import re
 import feedparser
 import urllib
 
-
-
-
-def download_torrent(link, title, torrent_folder):
-	urllib.urlretrieve (link, torrent_folder + "/" + title.replace(" ", ".") + ".torrent")
-
+watch_folder = '/volume1/script/test_watchfolder'
+url = 'http://www.torrentday.com/torrents/rss?download;11;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22' 
 list_of_stuff_i_want = ["Life.on.fire", "Austin.City.Limits", "Moving.On"]
 
 
-watch_folder = '/volume1/script/test_watchfolder'
-url = 'http://www.torrentday.com/torrents/rss?download;11;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22' 
+def download_torrent(link, title, torrent_folder):
+	urllib.urlretrieve (link + "wtf", torrent_folder + "/" + title.replace(" ", ".") + ".torrent")
+
+
+
 
 feed = feedparser.parse(url)
 for post in feed.entries:
