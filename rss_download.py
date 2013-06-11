3e49af5fcbd331fbbfac0024bc53343536312fdc
Christopher Hågensen <christopher.hagensen@gmail.com>
2013-06-11 09:23:16 +0200

    all new

--- rss_download.py
+++ rss_download.py
@@ -3,10 +3,12 @@
 import urllib
 import os
 
+distantfile = 'http://www.torrentday.com/torrents/rss?download;11;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22'
+localfile = './rss.temp'
+urllib.urlretrieve(distantfile, localfile)
 
-urllib.urlretrieve('http://www.torrentday.com/torrents/rss?download;11;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22' , 'rss.temp')
 
-if os.path.isfile(temp_rss.file):
+if os.path.isfile(./rss.temp):
 	print "file found";
 
 urllib.urlcleanup()
