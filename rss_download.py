3b3937b9ecb40f14438f4a19fd1200512153038c
Christopher Hågensen <christopher.hagensen@gmail.com>
2013-06-11 09:42:29 +0200

    import

--- rss_download.py
+++ rss_download.py
@@ -2,6 +2,7 @@
 
 import urllib
 import os
+import zlib
 
 distantfile = 'http://www.torrentday.com/torrents/rss?download;11;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22'
 localfile = './rss.temp'
