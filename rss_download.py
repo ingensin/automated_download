8e4d8f11686b744aea4859a049904f778761f224
Christopher Hågensen <christopher.hagensen@gmail.com>
2013-06-11 09:09:15 +0200

    update

--- rss_download.py
+++ rss_download.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-rss_server_url = 'http://www.torrentday.com/torrents/rss?download;11;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22'
+import urllib
 
-rss = urllib.urlretrive(rss_server_url , 'temp_rss.file')
-	print rss
+
+urllib.urlretrive('http://www.torrentday.com/torrents/rss?download;11;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22' , 'temp_rss.file')
 
 urllib.urlcleanup()
