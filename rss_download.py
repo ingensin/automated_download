14d45c6252751d6286cf3efeb3f91c556ad13e50
Christopher Hågensen <christopher.hagensen@gmail.com>
2013-06-11 20:29:27 +0200

    test

--- rss_download.py
+++ rss_download.py
@@ -3,15 +3,10 @@
 import feedparser
 import zlib
 
-python_wiki_rss_url = "http://www.torrentday.com/torrents/" \
-                      "rss?download;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22"
-decompressed_data=zlib.decompress(f.read( python_wiki_rss_url ), 16+zlib.MAX_WBITS)
+f = open('http://www.torrentday.com/torrents/rss?download;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22', 'rb')
+
+decompressed_data=zlib.decompress(f.read(), 16+zlib.MAX_WBITS)
 
 print decompressed_data
 
-feed = feedparser.parse( decompressed_data )
-
-
-   
-   
-   
+#feed = feedparser.parse( decompressed_data )
