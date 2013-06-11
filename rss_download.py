2a1227167f3bb5664806dc2f4f7509ea75d5de22
Christopher Hågensen <christopher.hagensen@gmail.com>
2013-06-11 09:15:56 +0200

    print

--- rss_download.py
+++ rss_download.py
@@ -6,6 +6,7 @@ import os
 
 urllib.urlretrive('http://www.torrentday.com/torrents/rss?download;11;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22' , 'temp_rss.file')
 
-if os.path.isfile(temp_rss.file): try: open(temp_rss.file)
+if os.path.isfile(temp_rss.file):
+	print "file found";
 
 urllib.urlcleanup()
