5e8e746b5319621572db5ab24181ddac11a1693a
Christopher Hågensen <christopher.hagensen@gmail.com>
2013-06-11 09:48:14 +0200

    .

--- rss_download.py
+++ rss_download.py
@@ -6,7 +6,7 @@ import zlib
 
 distantfile = 'http://www.torrentday.com/torrents/rss?download;11;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22'
 localfile = './rss.temp'
-urllib.urlretrieve(zlib.decompress(distantfile), localfile)
+urllib.urlretrieve(zlib.decompressobj(distantfile), localfile)
 
 
 if os.path.isfile('rss.temp'):
