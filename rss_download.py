4618786d7929df410353397923c06bc8cb6bb6a2
Christopher Hågensen <christopher.hagensen@gmail.com>
2013-06-11 09:17:15 +0200

    skrivefeil

--- rss_download.py
+++ rss_download.py
@@ -4,7 +4,7 @@ import urllib
 import os
 
 
-urllib.urlretrive('http://www.torrentday.com/torrents/rss?download;11;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22' , 'temp_rss.file')
+urllib.urlretrieve('http://www.torrentday.com/torrents/rss?download;11;7;u=428237;tp=887f3b1d10049f24d6fddf65d2139b22' , 'temp_rss.file')
 
 if os.path.isfile(temp_rss.file):
 	print "file found";
